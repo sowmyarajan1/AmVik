715a85fde8139e23751e3efa4e2e95005e8deba7
sowmyarajan1 <sowmyarajan@yahoo.com>
2018-09-07 10:32:57 -0400

    My First Project

---
+++ myfirst/src/com/icicibank/loans/carloans/MyFirstClass.java
@@ -0,0 +1,11 @@
+package com.icicibank.loans.carloans;
+
+public class MyFirstClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello");
+
+	}
+
+}
---
+++ myfirst/src/com/icicibank/loans/carloans/add.java
@@ -0,0 +1,10 @@
+package com.icicibank.loans.carloans;
+
+public class add {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
