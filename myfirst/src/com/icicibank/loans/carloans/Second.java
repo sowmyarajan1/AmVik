eb8ba2350286f2e6497dd26fd179a4d62c3d8d93
sowmyarajan1 <sowmyarajan@yahoo.com>
2018-09-11 10:45:59 -0400

    Added a new statement

--- myfirst/src/com/icicibank/loans/carloans/Second.java
+++ myfirst/src/com/icicibank/loans/carloans/Second.java
@@ -3,7 +3,8 @@ package com.icicibank.loans.carloans;
 public class Second {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("Hello!!");
 
 		
 		
